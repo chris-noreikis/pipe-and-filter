3187e6119c5ad4f664cb2a024d0df41504e0358b
Chris Noreikis <chris.noreikis@statsperform.com>
2019-11-06 21:26:03 -0600

    Remove unused imports

--- src/test/movies/PipeAndFilterRunnerTest.java
+++ src/test/movies/PipeAndFilterRunnerTest.java
@@ -4,12 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.*;
-
-import static org.hamcrest.CoreMatchers.hasItems;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 public class PipeAndFilterRunnerTest {
     private PipeAndFilterRunner g;
